dd612b2842932cedf82f312d291fc75f9b58cfd9
fakhryhizballah <falehry88@gmail.com>
2026-07-10 13:44:08 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers');
 
 router.post('/track/unique', controller.trackUniqueVisitor);
+router.post('/track/view', controller.trackView);
 router.get('/analytics/unique/:host', controller.analyticsUniqueVisitors);
 router.get('/analytics/track/:host/:date', controller.trackVisitor);
 
