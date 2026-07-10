6d3a941c404dfb0b2926c0cfbc9eedcd39254837
fakhryhizballah <falehry88@gmail.com>
2026-07-10 13:37:51 +0700

    update

---
+++ public/js/counter2.js
@@ -0,0 +1,84 @@
+function generateUniqueId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function getVisitorId() {
+    const visitorIdKey = 'visitor_id_session';
+    let visitorId = localStorage.getItem(visitorIdKey);
+
+    if (!visitorId) {
+        visitorId = generateUniqueId();
+        localStorage.setItem(visitorIdKey, visitorId);
+    }
+
+    return visitorId;
+}
+const currentVisitorId = getVisitorId();
+let getHostname = new URL(window.location);
+async function sha256(text) {
+    const buf = await crypto.subtle.digest(
+        "SHA-256",
+        new TextEncoder().encode(text)
+    );
+    return Array.from(new Uint8Array(buf))
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+}
+
+async function trackVisitor(host, visitorId) {
+    fetch('https://count.spairum.my.id/api/track/unique', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ host, visitorId })
+    })
+
+}
+console.log('Session Visitor ID:', currentVisitorId);
+async function trak(host) {
+    let keyCookie = await sha256(host);
+    const cookieExists = document.cookie.split(';').some(item => item.trim().startsWith(`${keyCookie}=`));
+    if (!cookieExists) {
+        trackVisitor(host, currentVisitorId);
+        document.cookie = `${keyCookie}=${currentVisitorId}; path=/; max-age=86400;`;
+    }
+}
+// Ganti 'websiteanda.com' dengan host yang sebenarnya
+trak(getHostname.hostname);
+trak(getHostname.hostname + '-' + getHostname.pathname.split('/')[1]);
+
+
+function getTrackUnique(host) {
+    return fetch('https://count.spairum.my.id/api/analytics/unique/' + host, {
+    }).then((response) => response.json());
+
+}
+// format date: YYYY-MM-DD;
+function getTrackbyDate(host, date) {
+    return fetch('https://count.spairum.my.id/api/analytics/track/' + host + '/' + date, {
+    }).then((response) => response.json());
+
+}
+async function views() {
+    let host = getHostname.href;
+    const cookieExists = document.cookie.split(';').some(item => item.trim().startsWith(`${host}=`));
+    if (!cookieExists) {
+        let result = await fetch('https://count.spairum.my.id/api/track/view', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ host })
+        })
+        result = await result.json();
+        console.log(result);
+        document.cookie = `${host}=${result.views}; path=/; max-age=60;`;
+    }
+
+}
+views()
